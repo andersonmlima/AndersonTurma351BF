57b182b64fcf4d633a90e0c819133dbeb22e9344
Anderson Moura <>
2023-08-29 20:40:08 -0300

    Desafio 1 em aula sobre classe

--- POO351BF/POO351BF/main.swift
+++ POO351BF/POO351BF/main.swift
@@ -7,5 +7,44 @@
 
 import Foundation
 
-print("Hello, World!")
+class Aviao {
+    var nome: String = "Jato"
+    var companhia: String = "Airbus"
+    var capacidade: Int = 80
+    
+    func voar() {
+        print("\(nome) da \(companhia) está voando com capacidade para \(capacidade) passageiros!")
+    }
+    
+    func pousar() {
+        print("\(nome) da \(companhia) está pousando.")
+    }
+}
 
+class Museu {
+    var nome: String = "Louvre"
+    var localizacao: String = "Paris"
+    var numeroExibicoes: Int = 50000
+    
+    func exibirExposicao() {
+        print("Bem vindo ao \(nome) em \(localizacao)! Temos \(numeroExibicoes) exposições para você conhecer.")
+    }
+    
+    func fechar() {
+        print("\(nome) em \(localizacao) está fechando suas portas por hoje.")
+    }
+}
+
+class Foguete {
+    var nome: String = "Apolo"
+    var fabricante: String = "Nasa"
+    var combustivel: String = "Hidrazina"
+    
+    func lancar() {
+        print("\(nome) fabricado pela \(fabricante) está sendo lançado ao espaço com combustível \(combustivel)!")
+    }
+    
+    func abortarLancamento() {
+        print("\(nome) teve o lançamento abortado.")
+    }
+}
