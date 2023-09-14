37aaa0385ec4a36fe8c0dbf5d10a6ab7e0c2325d
Anderson Moura <>
2023-09-14 15:36:27 -0300

    Desafio de telas

---
+++ Desafio0809/Desafio0809/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Desafio0809
+//
+//  Created by Anderson Moura on 14/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var enterButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func tappedEnterButton(_ sender: UIButton) {
+    }
+    
+}
+
